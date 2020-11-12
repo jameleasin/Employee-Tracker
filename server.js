6e2bbf029faf568c385e2ec5b8bf4507fb62d167
Jamie <Sinclair>
2020-11-12 11:33:04 +0800

    node modules

--- server.js
+++ server.js
@@ -2,3 +2,66 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 
+
+// create the connection information for the sql database
+var connection = mysql.createConnection({
+    host: "localhost",
+
+    // Your port; if not 3306
+    port: 3306,
+
+    // Your username
+    user: "root",
+
+    // Your password
+    password: "Volcom28!",
+    database: "employee_trackerDB"
+});
+
+
+
+
+// connect to the mysql server and sql database
+connection.connect(function (err) {
+    if (err) throw err;
+    start();
+});
+
+function start() {
+    inquirer
+      .prompt({
+        name: "action",
+        type: "list",
+        message: "What would you like to do?",
+        choices: [
+          "View all departments",
+          "View all roles",
+          "View all employees",
+          "Add a department",
+          "Add a role",
+          "Add an employee",
+          "Update employee role",
+          "Exit"
+        ]
+      })
+    .then(function(answer) {
+        if (answer.action === 'View all departments') {
+            viewDepartments();
+        } else if (answer.action === 'View all roles') {
+            viewRoles();
+        } else if (answer.action === 'View all employees') {
+            viewEmployees();
+        } else if (answer.action === 'Add a department') {
+            addDepartment();
+        } else if (answer.action === 'Add a role') {
+            addRole();
+        } else if (answer.action === 'Add an employee') {
+            addEmployee();
+        } else if (answer.action === 'Update employee role') {
+            updateRole();
+        }
+        else if (answer.action === 'Exit') {
+            connection.end();
+        }
+    })
+    
